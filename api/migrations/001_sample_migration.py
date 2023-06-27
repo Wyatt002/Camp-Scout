350ef55dbb857fecca6bc3b2d62db639dcab73a8
JennsReds <jenniferreds97@gmail.com>
2023-06-27 14:20:29 -0400

    merge test

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -22,7 +22,7 @@ steps = [
         CREATE TABLE account (
             id SERIAL PRIMARY KEY NOT NULL,
             first_name VARCHAR(50) NOT NULL,
-            last_name VARCAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
             email VARCHAR(50) NOT NULL,
             password VARCHAR(50) NOT NULL,
             avatar VARCHAR(200) NOT NULL,
@@ -33,7 +33,7 @@ steps = [
         """
         DROP TABLE account;
         """
-    ]
+    ],
     [
         # "Up"
         """
@@ -52,9 +52,9 @@ steps = [
         # "Down"
         """
         DROP TABLE profile;
-        """,
+        """
     ],
-        [
+    [
         # "Up"
         """
         CREATE TABLE review (
@@ -68,8 +68,6 @@ steps = [
         # "Down"
         """
         DROP TABLE profile;
-        """,
-    ],
-
-
+        """
+    ]
 ]

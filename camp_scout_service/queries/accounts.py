c60f306ac6955878fd4550df9d820d77ecf315dd
Brian <bwjohnson45@gmail.com>
2023-06-29 14:30:16 -0400

    accounts.py updated

--- camp_scout_service/queries/accounts.py
+++ camp_scout_service/queries/accounts.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from queries.pool import pool
 from jwtdown_fastapi.authentication import Token
 
+
 class Account(BaseModel):
     id: str
     username: str
@@ -9,32 +10,36 @@ class Account(BaseModel):
     email: str
     hashed_password: str
 
+
 class AccountIn(BaseModel):
     username: str
     fullname: str
     email: str
     password: str
 
+
 class AccountOut(AccountIn):
     id: str
+
 
 class AccountOutWithPassword(AccountOut):
     hashed_password: str
 
+
 class AccountQueries:
-    def get_one (self, email: str) -> Account:
+    def get_one(self, email: str) -> Account:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 result = db.execute(
                     """
                     SELECT id
                         , email
-                        , hashed_password
+                        , password
                         , full_name
                     FROM accounts
                     WHERE email = %s;
                     """,
-                    [email]
+                    [email],
                 )
                 record = result.fetchone()
                 if record is None:

f86a8a67f76c3dd0cb7e3582c5a2574aedeaca16
Connor <gariepct01@gmail.com>
2023-06-30 11:47:39 -0400

    Started to work on the pytest for create_account, still a WIP

---
+++ camp_scout_service/test_main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from authenticator import authenticator
+from fastapi import APIRouter
+from routers import accounts
+from main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_create_account():
+    response = client.post(
+        "/api/accounts",
+        json={
+            "password": "watchwatch",
+            "email": "watching@aol.com",
+            "first_name": "watcher",
+            "last_name": "who",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "access_token": "string",
+        "token_type": "Bearer",
+        "account": {
+            "password": "watchwatch",
+            "email": "watching@aol.com",
+            "first_name": "watcher",
+            "last_name": "who",
+        },
+    }

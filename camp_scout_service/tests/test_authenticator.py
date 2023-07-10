d5916537035c41a48ef5132836d47c5648748903
Connor <gariepct01@gmail.com>
2023-07-10 14:05:24 -0400

    Testing/playing around with unit tests, 1 success

--- camp_scout_service/tests/test_authenticator.py
+++ camp_scout_service/tests/test_authenticator.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from authenticator import authenticator
+import authenticator
 from fastapi import APIRouter
 from routers import accounts
 from main import app
@@ -14,11 +14,10 @@ def test_create_account():
     response = client.post(
         "/api/accounts",
         json={
-            "password": "watchwatch",
-            "email": "watching@aol.com",
-            "first_name": "watcher",
-            "last_name": "who",
+            "password": "testing",
+            "email": "test@aol.com",
+            "first_name": "tester",
+            "last_name": "testing",
         },
     )
     assert response.status_code == 200
-    assert response.json()

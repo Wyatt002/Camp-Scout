78bfa8343e3e3313b35f6b89dd1fac3ffb1210d4
Connor <gariepct01@gmail.com>
2023-07-11 12:14:49 -0400

    Weather api was grabbing weather every 3 hours instead of for everyday; adjusted to me a 5 day forecast every 24 hours from the time called

--- camp_scout_service/routers/weather.py
+++ camp_scout_service/routers/weather.py
@@ -32,24 +32,27 @@ async def get_forecast(lat: float, lon: float):
     }
     response = requests.get(weather_url, params=weather_params)
     if response.status_code == 200:
-        weather = {"1": {}, "2": {}, "3": {}, "4": {}}
+        weather = {}
+        num_day = 0
         content = response.json()
-        for day in range(0, 4):
-            info = {
-                "temp": content["list"][day]["main"]["temp"],
-                "temp_min": content["list"][day]["main"]["temp_min"],
-                "temp_max": content["list"][day]["main"]["temp_max"],
-                "feels_like": content["list"][day]["main"]["feels_like"],
-                "humidity": content["list"][day]["main"]["humidity"],
-                "weather": content["list"][day]["weather"][0]["main"],
-                "weather_description": content["list"][day]["weather"][0][
-                    "description"
-                ],
-                "weather_icon": content["list"][day]["weather"][0]["icon"],
-                "clouds": content["list"][day]["clouds"]["all"],
-                "wind": content["list"][day]["wind"]["speed"],
-                "visiblity": content["list"][day]["visibility"],
-                "date": content["list"][day]["dt_txt"],
-            }
-            weather[day + 1] = info
+        for day in range(0, 40):
+            if day == 0 or day == 8 or day == 16 or day == 24 or day == 32:
+                num_day += 1
+                info = {
+                    "temp": content["list"][day]["main"]["temp"],
+                    "temp_min": content["list"][day]["main"]["temp_min"],
+                    "temp_max": content["list"][day]["main"]["temp_max"],
+                    "feels_like": content["list"][day]["main"]["feels_like"],
+                    "humidity": content["list"][day]["main"]["humidity"],
+                    "weather": content["list"][day]["weather"][0]["main"],
+                    "weather_description": content["list"][day]["weather"][0][
+                        "description"
+                    ],
+                    "weather_icon": content["list"][day]["weather"][0]["icon"],
+                    "clouds": content["list"][day]["clouds"]["all"],
+                    "wind": content["list"][day]["wind"]["speed"],
+                    "visiblity": content["list"][day]["visibility"],
+                    "date": content["list"][day]["dt_txt"],
+                }
+                weather[num_day] = info
         return weather

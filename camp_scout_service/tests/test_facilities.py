066f68c920e1c2b9869aa7d50d44b7ac817de715
Connor <gariepct01@gmail.com>
2023-07-12 16:11:27 -0400

    Added extra validation to '/api/facilities' to check for a valid state_code from a list; created a new test to verify this with a success; updated 'docs/apis.md' to reflect changes to our response shape and other details

--- camp_scout_service/tests/test_facilities.py
+++ camp_scout_service/tests/test_facilities.py
@@ -12,6 +12,12 @@ def test_get_facilities():
     assert response.status_code == 200
 
 
+def test_bad_state_code():
+    response = client.get("/api/facilities", params={"state_code": "12"})
+    data = response.json()
+    assert data is None
+
+
 def test_no_empty_data_facilities():
     response = client.get("/api/facilities", params={"state_code": "NC"})
     data = response.json()
@@ -26,14 +32,4 @@ def test_get_facility_details():
         params={"facility_id": data["1"]["facility_id"]},
     )
     assert response.status_code == 200
-
-
-def test_no_empty_facility_details():
-    response = client.get("/api/facilities", params={"state_code": "NC"})
-    data = response.json()
-    response = client.get(
-        "/api/facility_details",
-        params={"facility_id": data["1"]["facility_id"]},
-    )
-    data = response.json()
     assert data is not None

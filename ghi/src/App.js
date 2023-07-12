16768adea0402fce78e8cf87910eda26a5e9f69e
JennsReds <jenniferreds97@gmail.com>
2023-07-12 20:03:17 +0000

    Login

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Construct from "./Construct.js";
-import ErrorNotification from "./ErrorNotification";
+//import Construct from "./Construct.js";
+//import ErrorNotification from "./ErrorNotification";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+
 import "./App.css";
 import SignupForm from "./signup.js";
+import LoginForm from "./login.js";
 
 
 function App() {
+  const baseUrl = process.env.REACT_APP_API_HOST
 // const [launchInfo, setLaunchInfo] = useState([]);
 //   const [error, setError] = useState(null);
 //   useEffect(() => {
@@ -27,15 +31,17 @@ function App() {
 //     getData();
   // }, []);
   return (
-    <BrowserRouter>
-    <div className="container">
-     <Routes>
-        <Route path="/accounts" >
-          <Route path="signup" element={<SignupForm />} />
-        </Route>
-     </Routes>
-    </div>
-    </BrowserRouter>
+    <AuthProvider baseUrl={baseUrl}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="accounts">
+            <Route path="signup" element={<SignupForm />} />
+            <Route path="login" element={<LoginForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+
   );
 }
 
